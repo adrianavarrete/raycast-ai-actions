594de0365451e5233eaf3e490e04706fcef412d5
Adria Navarrete Pardo <adri.navarrete.09@gmail.com>
2024-05-18 16:53:40 +0200

    Delete unneeded components

--- src/shared/ai_action_configuration.tsx
+++
@@ -1,5 +0,0 @@
-import { Detail } from '@raycast/api'
-
-export default function AiActionConfigurationComponent({ commandName }: { commandName: string }) {
-	return <Detail markdown={commandName} />
-}
