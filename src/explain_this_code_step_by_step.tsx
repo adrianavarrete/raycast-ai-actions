adfc86acd46addf7229193249fbb8412f007c497
Adria Navarrete Pardo <adri.navarrete.09@gmail.com>
2024-08-02 19:19:59 +0200

    Add new default command

---
+++ src/explain_this_code_step_by_step.tsx
@@ -0,0 +1,52 @@
+import { getPreferenceValues } from '@raycast/api'
+import { getAiAPIClient, getModel } from './shared/utils'
+import ExecuteCommand from './shared/execute_command/execute_command'
+
+const { commandCustomModelName } = getPreferenceValues()
+
+export default function ExplainThisCodeStepByStep() {
+	const { modelOwner, modelName, modelCode } = getModel(commandCustomModelName)
+
+	const prompt = `
+	Act as a software engineer with deep understanding of any programming language and it's documentation. Explain how the code works step by step in a list. Be concise with a casual tone of voice and write it as documentation for others.
+
+Code:
+
+	function GoToPreviousPageAction() {
+		const [page, setPage] = useGlobalState('page')
+		return (
+			<Action
+				icon={Icon.ArrowLeftCircle}
+				title="Go to Previous Page"
+				shortcut={{ modifiers: ['cmd'], key: '[' }}
+				onAction={() => setPage(Math.max(page - 1, 0))}
+			/>
+		)
+	}
+	;
+
+Explanation:
+The code is a React component that goes to the previous page.
+1. The component renders an 'Action' component with an icon, title, and shortcut.
+3. The 'useGlobalState' hook is used to get the current page number from the global state.
+4. The 'onAction' prop is used to set the page number to one less than the current page number.
+5. This will cause the page to go back one page when the action is taken.
+6. The page is capped at 0 so that the page is never negative.
+
+Code: {selection}
+
+Explanation:
+	`
+
+	const aiApiClient = getAiAPIClient(modelOwner)
+
+	return (
+		<ExecuteCommand
+			commandPrompt={prompt}
+			aiApiClient={aiApiClient}
+			modelOwner={modelOwner}
+			modelName={modelName}
+			modelCode={modelCode}
+		/>
+	)
+}
