c62f46bec75b8a5064cca201bc18bcf24cb02ea6
Adria Navarrete Pardo <adri.navarrete.09@gmail.com>
2024-08-04 13:30:06 +0200

    Implement a dropdown to select the avalable models

--- src/create_custom_command.tsx
+++ src/create_custom_command.tsx
@@ -1,5 +1,7 @@
+import { keys } from 'lodash'
 import { Action, ActionPanel, Form } from '@raycast/api'
 import { useForm, FormValidation } from '@raycast/utils'
+import { MODELS } from './shared/constants'
 
 type FormValues = {
 	prompt: string
@@ -32,10 +34,21 @@ export default function Command() {
 		</ActionPanel>
 	)
 
+	const availableModels = keys(MODELS)
+
 	return (
 		<Form actions={ActionView}>
-			<Form.TextField title="Model" placeholder="select model" {...itemProps.modelName} />
-			<Form.TextField title="Prompt" placeholder="Enter last name" {...itemProps.prompt} />
+			<Form.Dropdown title="Model" {...itemProps.modelName}>
+				{availableModels.map(model => {
+					return <Form.Dropdown.Item value={model} title={MODELS[model].NAME} key={model} />
+				})}
+			</Form.Dropdown>
+			<Form.TextField
+				title="Prompt"
+				placeholder="Write the prompt using {selection} for the selected text"
+				info="the key '{selection}' is required to refer to the selected text."
+				{...itemProps.prompt}
+			/>
 		</Form>
 	)
 }
